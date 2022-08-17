f19459db085c92f22ea07c0cc0f1028d2f9d26be
balakl123 <104009522+balakl123@users.noreply.github.com>
2022-08-17 15:01:12 +0530

    Update MainController.java

--- src/main/java/com/example/FirstSBProject/controller/MainController.java
+++ src/main/java/com/example/FirstSBProject/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
 	public static void main(String args[]) {
-		System.out.println("Hello World");
+		System.out.println("Welcome to Java");
 	}
 
 }
